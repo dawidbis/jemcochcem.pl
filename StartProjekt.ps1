715ce27d0ca58b05ccf92e06a9c55a4a48d66f31
macbondyra <149702525+macbondyra@users.noreply.github.com>
2026-05-06 21:13:08 +0200

    Update StartProjekt.ps1

--- StartProjekt.ps1
+++ StartProjekt.ps1
@@ -1,2 +1,2 @@
 Start-Process powershell "-Command cd FitApp; dotnet watch"
-Start-Process powershell "-Command cd fFitApp.client; npm run dev"
+Start-Process powershell "-Command cd FitApp.client; npm run dev"
